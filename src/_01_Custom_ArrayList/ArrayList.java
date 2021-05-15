a5b3ff39225ec750f4569dd59893432d60b859b4
Meiyi <hemeiyi0225@gmail.com>
2021-05-15 12:57:22 -0700

    ArrayList working on it

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,21 +3,28 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	T [] array;
 	public ArrayList() {
+		array=(T[]) new Object [0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		int number =0;
 		
-		return null;
+		
+		return array[loc];
 	}
 	
 	public void add(T val) {
-		
+		T[] big=(T[])new Object [array.length+1];
+		for(int i=0;i<array.length;i++) {
+			big[i]=array[i];
+		}
+		big[big.length-1]=val;
+		array=big;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
+
 		
 	}
 	
