6a0dbff401391325eeab759a00ab64e96bdab3f3
Meiyi <hemeiyi0225@gmail.com>
2021-05-22 12:55:27 -0700

    Arraylist finished

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -24,15 +24,32 @@ public class ArrayList <T>{
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-
+		
+		T[] big=(T[])new Object [array.length+1];
+		for(int i=0;i<loc;i++) {
+			big[i]=array[i];
+		}
+		big[loc]=val;
+		for(int i=loc+1;i<big.length;i++) {
+			big[i]=array[i-1];
+		}
+		array=big;
 		
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		array[loc]=val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
+		T[] big=(T[])new Object [array.length+1];
+		for(int i=0;i<loc;i++) {
+			big[i]=array[i];
+		}
+		for(int i=loc+1;i<big.length;i++) {
+			big[i]=array[i-1];
+		}
+		array=big;
 		
 	}
 	
